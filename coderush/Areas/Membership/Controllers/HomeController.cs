d8c3754ae7a120e851a0d540285c4267802447bf
ismail hamzah <gohamzah@outlook.com>
2018-07-23 13:25:43 +0700

    initial commit

---
+++ coderush/Areas/Membership/Controllers/HomeController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using coderush.Areas.Membership.ViewModels;
+using coderush.Data;
+using coderush.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coderush.Areas.Membership.Controllers
+{
+    [Area("Membership")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(ApplicationDbContext context,
+                        UserManager<ApplicationUser> userManager,
+                        SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Users.ToList());
+        }
+
+        // GET: Membership/Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Membership/Home/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Email,Password,ConfirmPassword")] RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    return RedirectToAction(nameof(Index));
+
+                }
+
+                AddErrors(result);
+                
+            }
+
+            return View(model);
+        }
+
+        // GET: Membership/Home/Edit/xxx
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+            return View(appUser);
+        }
+
+        // POST: Membership/Home/Edit/xxx
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Email")] ApplicationUser model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+                    if (appUser != null)
+                    {
+                        var email = appUser.Email;
+                        if (model.Email != email)
+                        {
+                            var setEmailResult = await _userManager.SetEmailAsync(appUser, model.Email);
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+            }
+            return View(model);
+        }
+
+        // GET: Membership/Home/Details/xxx
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(appUser);
+        }
+
+        // GET: Membership/Home/Delete/xxx
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(appUser);
+        }
+
+        // POST: Membership/Home/Delete/xxx
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+            var result = await _userManager.DeleteAsync(appUser);
+           
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Membership/Home/ChangePassword/xxx
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            ChangeUserPasswordViewModel changePassword = new ChangeUserPasswordViewModel();
+            changePassword.Id = appUser.Id;
+            changePassword.Email = appUser.Email;
+
+            return View(changePassword);
+        }
+
+        // POST: Membership/Home/ChangePassword/xxx
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(string id, [Bind("Id,Email,OldPassword,NewPassword,ConfirmPassword")] ChangeUserPasswordViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ApplicationUser appUser = await _userManager.FindByIdAsync(id);
+                    if (appUser != null)
+                    {
+                        var changePasswordResult = await _userManager.ChangePasswordAsync(appUser, model.OldPassword, model.NewPassword);
+                        if (!changePasswordResult.Succeeded)
+                        {
+                            AddErrors(changePasswordResult);
+                            ChangeUserPasswordViewModel changePassword = new ChangeUserPasswordViewModel();
+                            changePassword.Id = appUser.Id;
+                            changePassword.Email = appUser.Email;
+                            return View(changePassword);
+                        }
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
---
+++ coderush/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using coderush.Data;
+using coderush.Models;
+using coderush.Services;
+
+namespace coderush
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 0;
+                
+            })
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                        name: "areas",
+                        template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                      );
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
